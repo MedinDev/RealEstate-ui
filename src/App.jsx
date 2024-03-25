27922196d35c0deadcafb3fe9988f1888a89dbca
Medtex <medtexprog@gmail.com>
2024-03-26 00:55:01 +0800

    start day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import AddHouse from "./components/house/AddHouse.jsx";
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <AddHouse/>
+        </>
     );
 };
 
---
+++ src/components/common/HouseTypeSelector.jsx
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import {getHouseTypes} from "../utils/ApiFunctions.js";
+
+const HouseTypeSelector = ({handleHouseInputChange, newHouse}) => {
+    const [houseTypes, setHouseTypes] = useState([""])
+    const [showNewHouseTypeInput, setShowNewHouseTypeInput] = useState(false)
+    const [newHouseType, setNewHouseType] = useState("")
+
+    useEffect(() => {
+        getHouseTypes().then((data) => {
+            setHouseTypes(data)
+        })
+    }, [])
+
+    const handleNewHouseTypeInputChange = (e) => {
+        setNewHouseType(e.target.value)
+    }
+
+    const handleAddNewHouseType = () => {
+        if (newHouseType !== "") {
+            setHouseTypes([...houseTypes, newHouseType])
+            setNewHouseType("")
+            setShowNewHouseTypeInput(false)
+        }
+    }
+
+    return (
+        <>
+            {houseTypes.length > 0 && (
+                <div>
+                    <select
+                        required
+                        className="form-select"
+                        name="houseType"
+                        onChange={(e) => {
+                            if (e.target.value === "Add New") {
+                                setShowNewHouseTypeInput(true)
+                            } else {
+                                handleHouseInputChange(e)
+                            }
+                        }}
+                        value={newHouse.houseType}>
+                        <option value="">Select a house type</option>
+                        <option value={"Add New"}>Add New</option>
+                        {houseTypes.map((type, index) => (
+                            <option key={index} value={type}>
+                                {type}
+                            </option>
+                        ))}
+                    </select>
+                    {showNewHouseTypeInput && (
+                        <div className="mt-2">
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter New House Type"
+                                    value={newHouseType}
+                                    onChange={handleNewHouseTypeInputChange}
+                                />
+                                <button className="btn btn-hotel" type="button" onClick={handleAddNewHouseType}>
+                                    Add
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
+        </>
+    );
+};
+
+export default HouseTypeSelector;
